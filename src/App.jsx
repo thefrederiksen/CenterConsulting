1aa59167651c9803cf0bd808429a55d1af5fdfd3
Soren Frederiksen <soren@duksrevo.com>
2025-07-12 00:07:13 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import About from './components/About';
 import Services from './components/Services';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import './App.css';
 
 function App() {
   return (
